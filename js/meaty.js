fa07a258d66f16e11b04131d0ff2dfb28ba79181
Darkphantom786 <darkphantommt5@gmail.com>
2026-06-22 21:43:24 +0500

    meaty.js

--- js/meaty.js
+++ js/meaty.js
@@ -1,68 +1,7 @@
+alert("MEATY FILE LOADED");
+
 function initializeMeatyEngine() {
 
-    alert("Meaty Engine Loaded");
+    alert("MEATY FUNCTION CALLED");
 
-    const button =
-        document.getElementById("meatyButton");
-function initializeMeatyEngine() {
-
-    const button =
-        document.getElementById("meatyButton");
-
-    button.addEventListener("click", () => {
-
-        const normalAdvantage =
-            parseInt(
-                document.getElementById(
-                    "normalAdvantage"
-                ).value
-            );
-
-        const activeShift =
-            parseInt(
-                document.getElementById(
-                    "activeShift"
-                ).value
-            );
-
-        if (
-            isNaN(normalAdvantage) ||
-            isNaN(activeShift)
-        ) {
-
-            document.getElementById(
-                "meatyResults"
-            ).innerHTML =
-                "Enter Valid Values";
-
-            return;
-        }
-
-        const newAdvantage =
-            normalAdvantage +
-            activeShift;
-
-        document.getElementById(
-            "meatyResults"
-        ).innerHTML = `
-
-            <strong>Normal Advantage:</strong>
-            ${normalAdvantage}
-
-            <br><br>
-
-            <strong>Active Frame Shift:</strong>
-            ${activeShift}
-
-            <br><br>
-
-            <strong>New Meaty Advantage:</strong>
-            +${newAdvantage}
-
-        `;
-    });
-
-    console.log(
-        "Dynamic Meaty Engine Ready"
-    );
 }
