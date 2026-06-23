002c2983eb3a82619014b38f4748157a02c5e1b1
Darkphantom786 <darkphantommt5@gmail.com>
2026-06-23 15:27:03 +0500

    profile-engine.js

---
+++ js/profile-engine.js
@@ -0,0 +1,59 @@
+function initializeProfileEngine() {
+
+    const button =
+        document.getElementById(
+            "profileButton"
+        );
+
+    button.addEventListener(
+        "click",
+        async () => {
+
+            const name =
+                document.getElementById(
+                    "profileSelect"
+                ).value;
+
+            const data =
+                await loadProfiles();
+
+            const profile =
+                data.profiles.find(
+                    p => p.name === name
+                );
+
+            document.getElementById(
+                "profileResults"
+            ).innerHTML = `
+
+                <strong>Name:</strong>
+                ${profile.name}
+
+                <br><br>
+
+                <strong>Fastest DP:</strong>
+                ${profile.fastestDP}f
+
+                <br><br>
+
+                <strong>Fastest Super:</strong>
+                ${profile.fastestSuper}f
+
+                <br><br>
+
+                <strong>Hidden Gear:</strong>
+                ${profile.fastestHiddenGear}f
+
+                <br><br>
+
+                <strong>Guard Cancel:</strong>
+                ${profile.guardCancelStartup}f
+
+            `;
+        }
+    );
+
+    console.log(
+        "Opponent Profile System Ready"
+    );
+}
