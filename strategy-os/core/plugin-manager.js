84e78da2f320fd68c418c405e919b7d58d3e3505
Darkphantom786 <darkphantommt5@gmail.com>
2026-06-30 13:53:19 +0500

    Create plugin-manager.js

---
+++ strategy-os/core/plugin-manager.js
@@ -0,0 +1,60 @@
+class PluginManager {
+
+    constructor() {
+
+        this.plugins = new Map();
+
+    }
+
+    register(name, plugin) {
+
+        if (this.plugins.has(name)) {
+
+            console.warn(`Plugin already registered: ${name}`);
+            return;
+
+        }
+
+        this.plugins.set(name, plugin);
+
+        console.log(`Plugin Registered: ${name}`);
+
+    }
+
+    get(name) {
+
+        return this.plugins.get(name);
+
+    }
+
+    has(name) {
+
+        return this.plugins.has(name);
+
+    }
+
+    initializeAll() {
+
+        this.plugins.forEach(plugin => {
+
+            if (typeof plugin.initialize === "function") {
+
+                plugin.initialize();
+
+            }
+
+        });
+
+    }
+
+    list() {
+
+        return [...this.plugins.keys()];
+
+    }
+
+}
+
+const pluginManager = new PluginManager();
+
+export default pluginManager;
