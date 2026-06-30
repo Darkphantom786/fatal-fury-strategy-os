997b0289aa00ff96436558d97debc62cd347521a
Darkphantom786 <darkphantommt5@gmail.com>
2026-06-30 19:05:41 +0500

    saved

--- js/core/plugin-manager.js
+++ js/core/plugin-manager.js
@@ -1,60 +1,25 @@
 class PluginManager {
 
     constructor() {
-
         this.plugins = new Map();
-
     }
 
     register(name, plugin) {
-
-        if (this.plugins.has(name)) {
-
-            console.warn(`Plugin already registered: ${name}`);
-            return;
-
-        }
-
         this.plugins.set(name, plugin);
-
-        console.log(`Plugin Registered: ${name}`);
-
     }
 
     get(name) {
-
         return this.plugins.get(name);
-
-    }
-
-    has(name) {
-
-        return this.plugins.has(name);
-
     }
 
     initializeAll() {
-
         this.plugins.forEach(plugin => {
-
-            if (typeof plugin.initialize === "function") {
-
+            if (plugin && typeof plugin.initialize === "function") {
                 plugin.initialize();
-
             }
-
         });
-
-    }
-
-    list() {
-
-        return [...this.plugins.keys()];
-
     }
 
 }
 
-const pluginManager = new PluginManager();
-
-window.pluginManager = pluginManager;
+window.pluginManager = new PluginManager();
